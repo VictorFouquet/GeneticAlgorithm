e8c621971a3fb5445bae01f1921bb169d531da00
victorfouquet <victor.fouquet@gmail.com>
2023-02-17 20:00:18 +0100

    Comment Individual

--- src/GA/Individual.ts
+++ src/GA/Individual.ts
@@ -1,5 +1,9 @@
 import { _Individual } from "./interface/Individual.interface";
 
+/**
+ * Abstract class used to generate, store and cross DNA.
+ * All its method must be implemented in child class.
+ */
 export abstract class Individual<T> implements _Individual<T> {
     size: number;
     fitness: number;
