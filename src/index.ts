7f0f63e155abfe5fcf10513433cb867a54a7d8db
victorfouquet <victor.fouquet@gmail.com>
2023-02-23 00:22:02 +0100

    Update entry point

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { App } from "./TravellingSalesman/App";
 
-
 function main(): void {
-    const app: App = new App("canvas", 20);
-    app.run();
+    const CITY_COUNT:       number = 15;
+    const INDIVIDUAL_COUNT: number = 300;
+    const MUTATION_RATE:    number = 0.1
+    const app: App = new App(CITY_COUNT, INDIVIDUAL_COUNT, MUTATION_RATE);
 }
 
 main();
